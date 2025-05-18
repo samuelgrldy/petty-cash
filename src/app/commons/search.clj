b1b2fea43af046ab10a53247a82a1a8f881f88f0
samuelgrldy <samuelgeraldy@apps.ipb.ac.id>
2025-05-19 01:27:10 +0700

    refactor: remove unused functions and commented API code

--- src/app/commons/search.clj
+++ src/app/commons/search.clj
@@ -21,15 +21,6 @@
         st2-words (process-string st2)]
     (count (cset/intersection st1-words st2-words))))
 
-(defn all-skills
-  "Get all the skills from the db"
-  [db]
-  (mc/find-maps (:db db) "skills"))
-
-(defn all-sgs
-  "Get all the skill groups from the db"
-  [db]
-  (mc/find-maps (:db db) "sgroups" {:published true}))
 
 
 
